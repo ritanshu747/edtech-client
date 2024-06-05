e3d9b0c34ecfd4af39b815fd3a0fd86ff528e6b6
Ritanshu Shivhare <73697511+ritanshu747@users.noreply.github.com>
2024-06-05 16:03:55 +0530

    Update Navbar.jsx

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,331 +1,148 @@
-import React, { useEffect, useState } from 'react'
-import logo from '../../assets/Logo/Logo-Full-Light.png'
-import navbarLinks from '../../data/navbarLinks'
-import { Link, matchPath, useNavigate } from 'react-router-dom'
-import { SlArrowDown, SlArrowUp } from 'react-icons/sl'
-import { useLocation } from 'react-router-dom'
-import { AiOutlineShoppingCart, AiOutlineLogin, AiOutlineHome, AiOutlineContacts } from 'react-icons/ai';
-import { useDispatch, useSelector } from 'react-redux'
-import ProfileDropDown from '../core/Auth/ProfileDropDown'
-import { toast } from 'react-hot-toast'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import HamburgerMenu from './HamburgerMenu'
-import { VscDashboard, VscSignOut, VscSignIn } from "react-icons/vsc"
-import { logout } from '../../services/operations/authServices'
-import { BiCategory, BiDetail } from 'react-icons/bi'
-import { getAllCategories } from '../../services/operations/otherServices'
-import { getCurrentUser } from '../../services/operations/profileServices'
-import { setLoading } from '../../redux/slices/authSlice'
+import { useEffect, useState } from "react"
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
+import { BsChevronDown } from "react-icons/bs"
+import { useSelector } from "react-redux"
+import { Link, matchPath, useLocation } from "react-router-dom"
 
+import logo from "../../assets/Logo/Logo-Full-Light.png"
+import { NavbarLinks } from "../../data/navbar-links"
+import { apiConnector } from "../../services/apiconnector"
+import { categories } from "../../services/apis"
+import { ACCOUNT_TYPE } from "../../utils/constants"
+import ProfileDropdown from "../core/Auth/ProfileDropDown"
 
-const Navbar = () => {
-
-  const { token, loading } = useSelector((state) => state.auth)
+function Navbar() {
+  const { token } = useSelector((state) => state.auth)
   const { user } = useSelector((state) => state.profile)
-  const { cartItemsCount } = useSelector((state) => state.cart)
-  const [loading2, setLoading2] = useState(true);
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const { totalItems } = useSelector((state) => state.cart)
+  const location = useLocation()
 
-  const [catalogs, setCatalogs] = useState([]);
-  const [isMenuModalOpen, setIsMenuModalOpen] = useState(false);
-  const [categoryOpen, setCategoryOpen] = useState(false);
+  const [subLinks, setSubLinks] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    const fetchCatalog = async () => {
-      const toastId = toast.loading('Loading Backend ...');
-      const result = await getAllCategories();
-      if (result) {
-        setCatalogs(result)
-      } else {
-        toast.error('Failed to load backend');
+    ;(async () => {
+      setLoading(true)
+      try {
+        const res = await apiConnector("GET", categories.CATEGORIES_API)
+        setSubLinks(res.data.data)
+      } catch (error) {
+        console.log("Could not fetch Categories.", error)
       }
-      toast.dismiss(toastId);
-      setLoading2(false);
-    }
-    fetchCatalog();
-  }, []);
+      setLoading(false)
+    })()
+  }, [])
 
+  // console.log("sub links", subLinks)
 
-  useEffect(() => {
-    const getCurrentUserDetails = async () => {
-      setLoading(true);
-      if (token) {
-        await getCurrentUser(token, dispatch, navigate);
-      }
-      setLoading(false);
-    }
-    getCurrentUserDetails();
-  }, [token, dispatch, navigate]);
-
-  const matchRoute = (linkPath) => {
-    if (linkPath === '/') return matchPath({ path: linkPath }, location.pathname);
-    return location.pathname.startsWith(linkPath);
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname)
   }
-
-  const handleLogOutClick = async (e) => {
-    setIsMenuModalOpen(false);
-    await logout(token, dispatch, navigate);
-  }
-
-
-
 
   return (
-    <div className='bg-richblack-900 border-b border-b-richblack-700 h-14' >
-      <div className='w-11/12 h-14 mx-auto max-w-maxContent flex flex-row items-center justify-between' >
+    <div
+      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+        location.pathname !== "/" ? "bg-richblack-800" : ""
+      } transition-all duration-200`}
+    >
+      <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         {/* Logo */}
-        <div>
-          <Link to={'/'} >
-            <img src={logo} width={160} height={32} loading='lazy' alt="logo" />
-          </Link>
-        </div>
-
-        {/* Nav Links */}
-        <div className='' >
-          <nav className='hidden md:block' >
-            <ul className='flex gap-x-6 text-richblack-25' >
-              {
-                navbarLinks.map((link, ind) => (
-                  <li key={ind} >
-                    {
-                      link.title === 'Catalog'
-                        ?
-                        (
-                          <div className='flex items-center gap-1 group cursor-pointer relative ' >
-                            <p>{link.title}</p>
-                            <SlArrowDown className='translate-y-[1px]' />
-
-                            <div className='z-10 absolute top-[50%] translate-y-[3em] left-[50%] translate-x-[-50%] flex flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 transition-all duration-150 w-[200px]  lg:w-[300px] invisible  opacity-0  group-hover:visible group-hover:opacity-100 group-hover:translate-y-[1.65em]' >
-
-                              <div className='absolute h-6 w-6 top-0 translate-y-[-40%] select-none  left-[50%] translate-x-[80%] rotate-45 rounded bg-richblack-5'>
-                              </div>
-
-                              {
-                                catalogs.length ?
-                                  (
-                                    <div className='flex flex-col capitalize' >
-                                      {
-                                        catalogs.map((catalog, index) => (
-                                          <Link to={`/categorycourses/${catalog.name.split(' ').join('-')}`} key={index} >
-                                            <p className='hover:bg-richblack-50 rounded-lg py-3 pl-4' >{catalog.name}</p>
-                                          </Link>
-                                        ))
-                                      }
-                                    </div>
-                                  )
-                                  :
-                                  (<div className='select-none cursor-not-allowed' >No Catalog Available</div>)
-                              }
-                            </div>
-                          </div>
-                        )
-                        :
-                        (
-                          <Link to={link?.path} >
-                            <p className={`${matchRoute(link?.path) ? 'text-yellow-25' : 'text-richblack-25'}`} >{link.title}</p>
-                          </Link>
-                        )
-                    }
-                  </li>
-
-
-                ))
-              }
-            </ul>
-          </nav>
-        </div>
-
-        {/* Login / SignUp / DashBoard / Cart */}
-        <div className='hidden md:flex gap-x-4 items-center' >
-          {
-            (loading || loading2) &&
-            (
-              <div className='text-white font-bold'>
-                Loading ...
-              </div>
-            )
-          }
-          {
-            token === null &&
-            (
-              <Link to={'/login'} >
-                <button className='border border-richblack-700 bg-richblack-800 text-richblack-100 rounded-md px-3 py-2' >Log in</button>
-              </Link>
-            )
-          }
-
-          {
-            token === null &&
-            (
-              <Link to={'/signup'} >
-                <button className='border border-richblack-700 bg-richblack-800 text-richblack-100 rounded-md px-3 py-2' >Sign Up</button>
-              </Link>
-            )
-          }
-
-          {
-            user && user?.role === 'Student' &&
-            (
-              <Link to={'/dashboard/cart'} className='relative' >
-                <AiOutlineShoppingCart className='text-2xl text-richblack-100' />
-                {cartItemsCount > 0 && (
-                  <span className='absolute text-yellow-100 text-center text-xs font-bold bg-richblack-600 h-5 w-5 -bottom-2 -right-2 grid place-items-center rounded-full' >
-                    {cartItemsCount}
-                  </span>)}
-              </Link>
-            )
-          }
-
-          {
-            token !== null && (<ProfileDropDown />)
-          }
-        </div>
-
-
-        {/* HamberBurger Menu - only for small screen */}
-        <div className='mr-4 md:hidden' >
-          <GiHamburgerMenu
-            onClick={() => setIsMenuModalOpen(prev => !prev)}
-            className={` fill-richblack-100 `}
-            fontSize={24}
-          />
-
-          <HamburgerMenu
-            isMenuModalOpen={isMenuModalOpen}
-            setIsMenuModalOpen={setIsMenuModalOpen}
-          >
-            <div className='flex flex-col gap-y-2 py-5 px-5' >
-              {
-                (loading || loading2) &&
-                (
-                  <div className='text-white font-bold'>
-                    Loading ...
-                  </div>
-                )
-              }
-
-              {
-                token === null &&
-                (
-                  <Link to={'/login'} onClick={() => setIsMenuModalOpen(false)}   >
-                    <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 ' >
-                      <VscSignIn className='text-lg' />
-                      Log In
-                    </div>
-                  </Link>
-                )
-              }
-
-              {
-                token === null &&
-                (
-                  <Link to={'/signup'} onClick={() => setIsMenuModalOpen(false)} >
-                    <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 ' >
-                      <AiOutlineLogin className='text-lg' />
-                      Sign Up
-                    </div>
-                  </Link>
-                )
-              }
-
-              {
-                token !== null &&
-                (
-                  <Link to={'/dashboard/my-profile'} onClick={() => setIsMenuModalOpen(false)}  >
-                    <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 ' >
-                      <VscDashboard className='text-lg' />
-                      Dashboard
-                    </div>
-                  </Link>
-                )
-              }
-
-              {
-                token !== null && user && user?.role === 'Student' &&
-                (
-                  <Link to={'/dashboard/cart'} onClick={() => setIsMenuModalOpen(false)}  >
-                    <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 ' >
-                      <AiOutlineShoppingCart className='text-lg' />
-                      Cart
-                    </div>
-                  </Link>
-                )
-              }
-
-              {
-                token !== null &&
-                (
-                  <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 cursor-pointer'
-                    onClick={handleLogOutClick}>
-                    <VscSignOut className='text-lg' />
-                    LogOut
-                  </div>
-                )
-              }
-
-              {/* General Buttons */}
-              <div className='h-[1px] my-2 bg-richblack-100 w-3/4 mx-auto' ></div>
-
-              <Link to={'/'} onClick={() => setIsMenuModalOpen(false)}   >
-                <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 ' >
-                  <AiOutlineHome className='text-lg' />
-                  Home
-                </div>
-              </Link>
-
-              <Link to={'/about'} onClick={() => setIsMenuModalOpen(false)}   >
-                <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 ' >
-                  <BiDetail className='text-lg' />
-                  About Us
-                </div>
-              </Link>
-
-              <Link to={'/contact'} onClick={() => setIsMenuModalOpen(false)} >
-                <div className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 hover:text-richblack-25 hover:bg-richblack-700 ' >
-                  <AiOutlineContacts className='text-lg' />
-                  Contact Us
-                </div>
-              </Link>
-
-              {/* Category */}
-              <div className=''
-                onClick={() => setCategoryOpen(prev => !prev)}
-              >
-                <details >
-                  <summary className='flex gap-x-2 items-center w-full py-2 px-3 text-richblack-100 ' >
-                    <BiCategory className='text-lg' />
-                    Category
-                    {
-                      categoryOpen ? <SlArrowUp className='translate-y-[1px] ml-auto mr-1' /> : <SlArrowDown className='translate-y-[1px] ml-auto mr-1' />
-                    }
-                  </summary>
-
-                  <div className='px-4 text-richblack-100' >
-                    {
-                      catalogs.length ?
-                        (
-                          <div className='flex flex-col capitalize' >
-                            {
-                              catalogs.map((catalog, index) => (
-                                <Link to={`/categorycourses/${catalog.name.split(' ').join('-')}`} key={index} onClick={() => setIsMenuModalOpen(false)} >
-                                  <p className=' rounded-lg py-2 pl-4' >{catalog.name}</p>
+        <Link to="/">
+          <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
+        </Link>
+        {/* Navigation links */}
+        <nav className="hidden md:block">
+          <ul className="flex gap-x-6 text-richblack-25">
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <>
+                    <div
+                      className={`group relative flex cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      <p>{link.title}</p>
+                      <BsChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : (subLinks && subLinks.length) ? (
+                          <>
+                            {subLinks
+                              ?.filter(
+                                (subLink) => subLink?.courses?.length > 0
+                              )
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.name}</p>
                                 </Link>
-                              ))
-                            }
-                          </div>
-                        )
-                        :
-                        (<div className='rounded-lg py-2 pl-4 select-none cursor-not-allowed' >No Catalog Available</div>)
-                    }
-                  </div>
-                </details>
-              </div>
-            </div>
-          </HamburgerMenu>
+                              ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        {/* Login / Signup / Dashboard */}
+        <div className="hidden items-center gap-x-4 md:flex">
+          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/login">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Log in
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/signup">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Sign up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropdown />}
         </div>
-
+        <button className="mr-4 md:hidden">
+          <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
+        </button>
       </div>
     </div>
   )
